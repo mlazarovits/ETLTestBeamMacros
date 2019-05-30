dc87959690b700a63835547bdd3e427e0c230af9
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-05-30 12:57:27 -0500

    changed y offset for 1D profiles

--- macros/plot_HPK3p1_4x4_4e14_450V.C
+++ macros/plot_HPK3p1_4x4_4e14_450V.C
@@ -9,15 +9,15 @@ int main(int argc, char **argv)
 	mp.chainPath = "root://cmseos.fnal.gov//store/group/cmstestbeam/2019_04_April_CMSTiming/KeySightScope/RecoData/TimingDAQRECO/RecoWithTracks/v1/confInfo/";
 	mp.debug=false;
 	//Define run range
-	mp.run_start = new vector<int>{13245,13249};
-	mp.run_end = new vector<int>{13248,13370};
+	mp.run_start = new vector<int>{13245};
+	mp.run_end = new vector<int>{13370};
 	//Note: will try to load every run in this range, even if it doesn't exist (so, expect some harmless complaints.)
 
 	//Define rotation angle and manual adjustments of x and y
   	float theta = 1.909; //from spreadsheet
-	mp.angle = new vector<float>{-theta,-theta}; //degrees
-	mp.x_offset= new vector<float>{0.040,0.040}; //mm
-	mp.y_offset= new vector<float>{0.0,0.0}; //mm
+	mp.angle = new vector<float>{-theta}; //degrees
+	mp.x_offset= new vector<float>{0.040}; //mm
+	mp.y_offset= new vector<float>{0.050}; //mm
 
 	//Define xy binning and ranges [mm]
 	mp.nbinsX=160;//30;
@@ -34,15 +34,15 @@ int main(int argc, char **argv)
 	mp.minTime=6.0e-9; mp.maxTime=7.0e-9;
 
 	//define threshold for LGAD hits, and range for photek
-	mp.hitThres= 30.;
+	mp.hitThres=30.;
 	mp.photekMin=20;
 	mp.photekMax=60;
 
 	// Define geometric boundaries for 1D "slices"
-	mp.xSliceMin={5.1,8,11,14};
-	mp.xSliceMax={7.7,10.8,13.8,16.8};
-	mp.ySliceMin={32.2,33.1,34.1,35.1};
-	mp.ySliceMax={32.9,33.9,34.9,35.9};
+	mp.xSliceMin={6,9,12,15};
+	mp.xSliceMax={8.2,11.2,14.2,17.2};
+	mp.ySliceMin={32.6,33.6,34.6,35.6};
+	mp.ySliceMax={33,34,35,36};
 
 	//Specify range for colz maps. -1 is ignored.
 	mp.zMinEff=-1.; mp.zMaxEff=-1.; 
