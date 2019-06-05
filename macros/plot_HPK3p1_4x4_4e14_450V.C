617f7d677a5992d9e72f38c6e061d878e1de808d
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-06-04 22:02:05 -0500

    changed 500v amp range to 0 - 80 mV

--- macros/plot_HPK3p1_4x4_4e14_450V.C
+++ macros/plot_HPK3p1_4x4_4e14_450V.C
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 	mp.minTime=6.0e-9; mp.maxTime=7.0e-9;
 
 	//define threshold for LGAD hits, and range for photek
-	mp.hitThres=30.;
+	mp.hitThres=20.;
 	mp.photekMin=10;
 	mp.photekMax=80;
 
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
 	mp.zMinEff=-1.; mp.zMaxEff=-1.; 
 	mp.zMinGain=10.; mp.zMaxGain=60.; 
 	mp.zMinSigmat=0.01e-9; mp.zMaxSigmat=0.09e-9; //raise z-axis
-	mp.zMinDeltat=4e-9; mp.zMaxDeltat=8e-9;	
+	mp.zMinDeltat=4e-9; mp.zMaxDeltat=9e-9;	
 
 	//Do everything
     mp.makeMaps();
