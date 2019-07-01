79f9f2acb49a113aac319e5d9584bcefec6e8941
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-07-01 14:06:20 -0500

    updated zaxis for normalized plots

--- macros/plot_HPK3p1_4x4_4e14_500V.C
+++ macros/plot_HPK3p1_4x4_4e14_500V.C
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	mp.y_offset= new vector<float>{0.1}; //mm
 	
 	//Define xy binning and ranges [mm]
-	mp.nbinsX=160;//30;
+	mp.nbinsX=80;//30;
 	mp.nbinsY=80;//10;
 	mp.rebinFactor=2; //coarser binning for maps of gain, timing (finer for efficiency)
 	mp.minX=4; mp.maxX=20;
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 
 	//define threshold for LGAD hits, and range for photek
 	mp.hitThres=vector<float>(mp.npad+1,30.);
-	// mp.hitThres[15] = 80.;
+	mp.hitThres[15] = 80.;
 	mp.photekMin=10;
 	mp.photekMax=100;
 
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 	mp.zMinEff=-1.; mp.zMaxEff=-1.; 
 	mp.zMinGain=0.; mp.zMaxGain=80.; 
 	mp.zMinSigmat=0.01e-9; mp.zMaxSigmat=0.1e-9; 
-	mp.zMinDeltat=4e-9; mp.zMaxDeltat=-1.;
+	mp.zMinDeltat=6e-9; mp.zMaxDeltat=-1.;
 
 
 	//Do everything
