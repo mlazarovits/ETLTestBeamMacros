701452f2bf5f9b1c88cde00276a0d82d7715d6a0
Margaret Lazarovits <mlazarovits@gmail.com>
2020-01-23 16:00:43 -0600

    debug optimizer class compiling

--- src/calcDropoffs.C
+++ src/calcDropoffs.C
@@ -4,7 +4,7 @@ using namespace std;
 #include <vector>
 
 // typedef pair<Double_t,Double_t> pairs;
-std::set<Double_t,Double_t> calcDropoffs(TH1* hist){
+set<Double_t,Double_t> calcDropoffs(TH1* hist){
 	// TFile* file = TFile::Open("~/ETLTestBeamMacros/output/HPK3p1_4x4_prerad/HPK3p1_4x4_prerad.root");
 	// TString histname = "h_x_eff_0_2";
 
